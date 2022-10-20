9776a4ec6d4c62fe10d966d26b07653e7a098bcc
testcopy125 <116233190+testcopy125@users.noreply.github.com>
2022-10-20 23:06:08 +0530

    Update Catpost.js

--- Catpost.js
+++ Catpost.js
@@ -1,11 +1,1 @@
-var dr = document.querySelectorAll("pmt");
-function labelthumbs(e){ output ='<ul>';
-for(var t=0;t<numpostse;t++){var i,r=e.feed.entry[t],l=r.title.$t;
-if(t==e.feed.entry.length)break;
-var output;
-for(var n=0;n<r.link.length;n++){if("replies"==r.link[n].rel&&"text/html"==r.link[n].type)r.link[n].title,r.link[n].href;if("alternate"==r.link[n].rel){i=r.link[n].href;break}}
-output= output + '<li id="linktps">';
-output=  output + '<a href="'+i+'" id="ltadre" target ="_top">'+l+"</a>";
-output=  output + "</li>";}
-output=  output + "</ul>";
-document.write(output);
+
